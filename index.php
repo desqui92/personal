cdfbe2dc7f3a5f04452518fd0710dcf182f94507
ElChukistrukis <coro99o@hotmail.com>
2020-05-03 18:16:49 -0500

    error

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <DOCTYPE html>
 <html>
 <head>
-<link href="../css/bootstrap.min.css" rel="stylesheet">
-<script src="../js/jquery.js">
-<script src="../js/bootstrap.min.js">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<script src="js/jquery.js">
+<script src="js/popper.min.js">
+<script src="js/bootstrap.min.js">
 </head>
 <body>
+<button type="button" class="btn btn-primary">Primary</button>
 Hola
 </body>
 </html>
