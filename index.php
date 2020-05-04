b9b855718356e901514fe87b262c12d74552681f
desqui92 <desqui92@gmail.com>
2020-05-04 03:18:00 -0300

    cxcvx

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <DOCTYPE html>
 <html>
 <head>
+<<<<<<< HEAD
 <link href="Vista/css/estilo.css" rel="stylesheet">
 <link href="Vista/css/bootstrap.min.css" rel="stylesheet">
 <script src="Vista/js/jquery.js"></script>
@@ -11,6 +12,14 @@
 <?php
 include "Vista/Modulos/Menu.php";
 ?>
+=======
+<link  rel="stylesheet" href="css/bootstrap.min.css">
+<script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+>>>>>>> cxcvx
 
 </body>
 </html>
