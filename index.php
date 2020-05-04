5db0d55bc35e0f503f77d1613751e85cb8acaf3f
desqui92 <43731462+desqui92@users.noreply.github.com>
2020-05-04 04:51:57 -0300

    ñññ

---
+++ index.php
@@ -0,0 +1,16 @@
+<DOCTYPE html>
+<html>
+<head>
+<link href="Vista/css/estilo.css" rel="stylesheet">
+<link href="Vista/css/bootstrap.min.css" rel="stylesheet">
+<script src="Vista/js/jquery.js"></script>
+<script src="Vista/js/popper.min.js"></script>
+<script src="Vista/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php
+include "Vista/Modulos/Menu.php";
+?>
+
+</body>
+</html>
